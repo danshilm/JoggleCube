9039ae78155b331eceb33caf875055492d0899ae
jty0 <36884464+jty0@users.noreply.github.com>
2018-05-03 12:20:52 +0400

    Update gameMainClass.java

--- src/joggleGame/src/uk/ac/aber/cs221/aumgroup/gameFrames/gameMainClass.java
+++ src/joggleGame/src/uk/ac/aber/cs221/aumgroup/gameFrames/gameMainClass.java
@@ -1,20 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * @gameMainClass.java 1.1 2018/05/04
+ *
+ * Copyright (c) 2018 Aberystwyth University.
+ * All rights reserved.
+ *
  */
+
 package uk.ac.aber.cs221.aumgroup.gameFrames;
 
 /**
+ * The gameMainClass load the components for the startmenu page to be display
  *
- * @author MY PC
+ * @author the12, jty
  */
 public class gameMainClass {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // This load the startmenu page when game is run.
-            startMenu sM = new startMenu();
-            sM.setVisible(true);
+        startMenu sM = new startMenu();
+        // check if the startmenu is visible
+        sM.setVisible(true);
 
-         }
-    
+    }
+
 }
+
